5190d07ec94c302c61bf45cb813727bb7b6d60f0
Arpan NLN <arpannln@ucla.edu>
2018-04-30 10:11:31 -0700

    woo

--- containers/App.js
+++ containers/App.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
+import '../styling/App.css';
 
 export default class App extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        PLAYER_ONE_SYMBOL: "X",
+        PLAYER_TWO_SYMBOL: "O",
+        currentTurn: "X",
+        board: ["", "", "", "", "", "", "", "", ""]
+
+      };
+    }
+
     render () {
         return <p>This is my new react app</p>;
     }
